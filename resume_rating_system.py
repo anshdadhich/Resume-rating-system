fbd722a53526846e5d18c94a227f02a0f04868aa
anshdadhich <anshdadhichmm@gmail.com>
2025-02-09 15:34:43 +0530

    Update resume_rating_system.py

--- resume_rating_system.py
+++ resume_rating_system.py
@@ -23,29 +23,36 @@ if type(min_score) == float or int:
     
     for resume in resume_text:
         score,skills_not_found = rate_resume(str(resume_text[resume]),description)
-        resume_text[resume] = [resume_text[resume], str(score),str(skills_not_found)]
+        resume_text[resume] = [resume_text[resume], str(score),str(skills_not_found)]  
     
-    st.write("Filtered resumes according to score : ")
     sorted_resumes = sorted(resume_text,key = lambda x: x[1])
     
     seleted_resumes = 0
     
     done = False
-    for sr in sorted_resumes:
-        if resume_text[sr][1] > min_score:
-            st.code(sr + " - " + resume_text[sr][1])
-            seleted_resumes += 1
-        if sr == sorted_resumes[-1]: 
-           done = True
-    
+    if min_score.strip() != "":
+        if description.strip() != "":
+           st.write("Filtered resumes according to score : ")
+           for sr in sorted_resumes:
+               if resume_text[sr][1] > min_score:
+                   st.code(sr + " : " + resume_text[sr][1])
+                   seleted_resumes += 1
+               if sr == sorted_resumes[-1]: 
+                  done = True
+        else:
+            st.write("please provide a description")
+    else:
+        st.write("please enter minimun score")
+
     if done == True and seleted_resumes == 0:
        st.code("All resume scored below than required score")
         
     st.write("\n\n\n")
-        
-    st.write("All resume with scores: ")
-    for res in resume_text:
-        st.code(f"Name: {res} \nscore : {resume_text[res][1]}\nskills not found : {(resume_text[res][2])}")
+    
+    if description.strip() != "":   
+        st.write("All resume with scores: ")
+        for res in resume_text:
+            st.code(f"Name: {res} \nscore : {resume_text[res][1]}\nskills not found : {(resume_text[res][2])}")
 
 else:
     st.header("Make sure the minimun score is a number")
